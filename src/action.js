74687300f92ed65063b8b2d324b2ac827be32a72
qtchaos <72168435+qtchaos@users.noreply.github.com>
2022-05-14 10:16:09 +0300

    update GitHub action

--- src/action.js
+++ src/action.js
@@ -3,17 +3,16 @@ const github = require("@actions/github");
 
 async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
   const { context = {} } = github;
-  const { pull_request = {} } = context.payload;
+  const { pull_request } = context.payload;
 
   await octokit.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: "Thank you for submitting a pull request, we'll review this as soon as possible.",
-  })
+    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
+  });
 }
 
-run()
+run();
