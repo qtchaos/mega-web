b8cfbf2dd78b2f3738acec0f74d3443f25ca0e86
qtchaos <72168435+qtchaos@users.noreply.github.com>
2022-05-14 10:45:06 +0300

    Add index.html

--- src/action.js
+++ src/action.js
@@ -1,19 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
 async function run() {
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-
-  const octokit = github.getOctokit(GITHUB_TOKEN);
-
-  const { context = {} } = github;
-  const { pull_request } = context.payload;
-
-  await github.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: "Thank you for submitting a pull request! We will try to review this as soon as we can."
-  });
+  console.log("Running action");
 }
 
 run();
